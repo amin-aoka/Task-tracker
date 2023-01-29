ab9ce34b2da11c64317514c2dff00242447dc0f5
Amin Koupaie <48063971+amin-aoka@users.noreply.github.com>
2023-01-29 11:20:24 +0000

    usestate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+
+import { useState } from 'react';
 import Header from './components/Header';
 import Button from './components/Button';
 import Tasks from './components/Tasks';
 
 function App() {
+    const[tasks,setTasks]= useState( [   
+    {
+        id:1,
+        text:"Study Math",
+        day: 'Feb 05th at 02:30',
+        reminder:false,
+    },
+    {
+        id:2,
+        text:"Go gym",
+        day: 'March 05th at 07:00',
+        reminder:true,
+    },
+    {
+        id:3,
+        text:"Food Shopping",
+        day: 'Jan 29th at 18:00',
+        reminder:false,
+    }]
+)
+
   return (
     <div className="container">
 
       <Header title ="Task Tracker"/>
-      <Button text ="add" color = "green"/>   
-      <Tasks/>
+      <Button text ="add" color = "green"/> 
+      <Tasks tasks={tasks}/>
+      
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,11 @@
 
-const tasks = [
-    {
-        id:1,
-        text:"Study Math",
-        day: 'Feb 05th at 02:30',
-        reminder:false,
-    },
-    {
-        id:2,
-        text:"Go gym",
-        day: 'March 05th at 07:00',
-        reminder:true,
-    },
-    {
-        id:3,
-        text:"Food Shopping",
-        day: 'Jan 29th at 18:00',
-        reminder:false,
-    },
-];
-
-
-function Tasks (){
+export default function Tasks ({tasks}){
+    
     return(
-
-        tasks.map((task)=> {
-            <h3>{task.text}</h3>
-        })
-    )
-}
-
-export default Tasks
+        <>
+          { tasks.map((task)=>{ 
+            
+                return <h3 key ={task.id}>{task.text}</h3>
+            })}  
+        </>
+    )}
